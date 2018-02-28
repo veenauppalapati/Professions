063adb5c6e3ce9a45da69c9082f3f2116bb4f76b
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-02-28 02:01:09 -0500

    refersnce for inserting values

--- professiontable_test/seeders/20180228065555-my-seed-file.js
+++ professiontable_test/seeders/20180228065555-my-seed-file.js
@@ -2,25 +2,19 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('Users', [{
+      first_name : 'John',
+      last_name : 'Doe',
+      bio : 'I am a new user to this application',
+      createdAt : new Date(),
+      updatedAt : new Date(),
+      email : 'johnDoe@test.com'
+    }], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
+    queryInterface.bulkDelete('Users', [{
+      first_name :'John'
+    }])
   }
 };
