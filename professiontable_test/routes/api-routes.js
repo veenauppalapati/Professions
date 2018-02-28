17e7193e5a9e0f85fb27ed220846f6d98ba79bd0
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-02-28 11:52:54 -0500

    query method for get request

--- professiontable_test/routes/api-routes.js
+++ professiontable_test/routes/api-routes.js
@@ -12,10 +12,20 @@ module.exports = function(app) {
     // parameter res is there so we can send back a response to the client 
     app.get('/professions', function(req, res){
         // for the purpose of finding id and occupation, we are going to use findAll method
-        //Example
-        Model.findAll({
-            attributes: ['foo', 'bar']
-          });
+        // we are using db to get access to the models
+        // name of the model we are going to use
+        // method we are going to use in this case find all 
+        // leave the object empty cuze we are not looking for anything specific 
+        db.Profession.findAll({});
         
     });
 };
+
+//example to refer to
+  // GET route for getting all of the posts
+  app.get("/api/posts/", function(req, res) {
+    db.Post.findAll({})
+    .then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
