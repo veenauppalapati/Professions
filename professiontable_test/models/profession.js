232339f59a5c1908b987ce32d458439aeac8a776
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-02-28 01:30:38 -0500

    Profession table is created

--- professiontable_test/models/profession.js
+++ professiontable_test/models/profession.js
@@ -1,25 +1,14 @@
 module.exports = function(sequelize, DataTypes) {
-
-    // Creates a table "profession" in our network database
-      var Profession = sequelize.define("Profession", {
-  
-        // LIST OF COLUMNS //
-        //Category is a string type, cannot be null, and validates for length
-        category: {
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-            len: [1, 160]
-          }
-        }, 
-       
-      });
-
-      Profession.associate = function (models) {
-        Profession.belongsToMany(models.User, {
-          through:  'user_profession'
-        });
-      };
-
-      return Profession;
-    };
+// Creates a table "profession" in our network database
+  var Profession = sequelize.define("Profession", {
+    // LIST OF COLUMNS //
+    occupation: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 160]
+      }
+    }   
+  });
+  return Profession;
+};
