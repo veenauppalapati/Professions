bb6291d337f8707c1e070d4b04b3d041d551947e
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-02-28 11:46:17 -0500

    questions to ask myself for get request

--- professiontable_test/routes/api-routes.js
+++ professiontable_test/routes/api-routes.js
@@ -11,6 +11,14 @@ module.exports = function(app) {
     // request coming from the client is stored in the parameter req
     // parameter res is there so we can send back a response to the client 
     app.get('/professions', function(req, res){
+        // what is the purpose of this get request?
+        // the purpose of it is to grab data from the database 
+        // how do we grab data from database
+        // we have to make a query to database
+        // how do we make a query to database
+        // we are using sequelize so we are going to make a query using sequelize documentation
+        // for the purpose of finding id and occupation, we are going to use findAll method
+        
 
     });
 };
