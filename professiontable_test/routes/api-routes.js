1c77ebde4d3d6c482c4025a480c29486b138545f
veena uppalapati <Veena@veenas-MBP.fios-router.home>
2018-02-28 11:39:56 -0500

    get route starter code

--- professiontable_test/routes/api-routes.js
+++ professiontable_test/routes/api-routes.js
@@ -4,4 +4,13 @@ var db = require("../models");
 // Routes
 // =============================================================
 module.exports = function(app) {
+    //GET ROUTE
+    //=========================================
+    // get request 
+    // listen to it via /professions
+    // request coming from the client is stored in the parameter req
+    // parameter res is there so we can send back a response to the client 
+    app.get('/professions', function(req, res){
+
+    });
 };
